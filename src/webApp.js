212676e95d0f577198febfcb82807c6a8e5af1eb
Santiago Ramirez Castellanos <santiago5.ramirez@ucp.edu.co>
2024-10-22 12:43:15 -0500

    new Commit Oct 22 V6

--- src/webApp.js
+++ src/webApp.js
@@ -1,26 +1,36 @@
+
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
-import taskRoutes from "./routes/task.routes.js";
-import cors from "cors";
+import taskRoutes from "./routes/tasks.routes.js";
 import { FRONTEND_URL } from "./config.js";
 
 const app = express();
 
 app.use(
   cors({
+    credentials: true,
     origin: FRONTEND_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true
   })
 );
-app.options('*', cors()); // Manejar preflight requests con OPTIONS
-app.use(morgan("dev"));
 app.use(express.json());
+app.use(morgan("dev"));
 app.use(cookieParser());
 
 app.use("/api", authRoutes);
 app.use("/api", taskRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const path = await import("path");
+  app.use(express.static("agent/dist"));
+
+  app.get("*", (req, res) => {
+    console.log(path.resolve("agent", "dist", "index.html") );
+    res.sendFile(path.resolve("agent", "dist", "index.html"));
+  });
+}
 
 export default app;

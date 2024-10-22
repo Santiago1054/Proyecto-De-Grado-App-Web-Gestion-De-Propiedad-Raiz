ccc18d05ab63dd35c3545be20df4e2ac37ec9d23
Santiago Ramirez Castellanos <santiago5.ramirez@ucp.edu.co>
2024-10-22 13:49:26 -0500

    new Commit Oct 22 V9

--- src/config.js
+++ src/config.js
@@ -1,11 +1,6 @@
 
-import dotenv from "dotenv";
 export const PORT = process.env.PORT ||4000;
 
-
-
-dotenv.config(); // Cargar variables de entorno desde el archivo .env
-
-export const TOKEN_SECRET = process.env.TOKEN_SECRET; // Asegúrate de que TOKEN_SECRET esté en el archivo .env
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "https://mern-stack-crud-with-jwt-1.onrender.com";
--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,15 +1,18 @@
 import { TOKEN_SECRET } from "../config.js";
 import jwt from "jsonwebtoken";
 
-export async function createAccessToken(payload) {
-  try {
-    const token = await jwt.sign(
+export function createAccessToken(payload) {
+  return new Promise((resolve, reject) => {
+    jwt.sign(
       payload,
       TOKEN_SECRET,
-      { expiresIn: "1d" }
+      {
+        expiresIn: "1d",
+      },
+      (err, token) => {
+        if (err) reject(err);
+        resolve(token);
+      }
     );
-    return token;
-  } catch (err) {
-    throw new Error(err); // Manejo de errores
-  }
+  });
 }

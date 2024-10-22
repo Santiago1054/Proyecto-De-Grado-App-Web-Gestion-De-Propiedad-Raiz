7c714e1e7cdd17a48637962c50ca4be4b33b5d19
Santiago Ramirez Castellanos <santiago5.ramirez@ucp.edu.co>
2024-10-22 09:05:36 -0500

    new Commit 22

--- agent/src/config.js
+++ agent/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL|| "https://mern-stack-crud-with-jwt.onrender.com/api";
+export const API_URL = import.meta.env.VITE_API_URL|| "http://localhost:4000/api";
--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ export const PORT = process.env.PORT ||4000;
 
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "https://mern-stack-crud-with-jwt-1.onrender.com";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";

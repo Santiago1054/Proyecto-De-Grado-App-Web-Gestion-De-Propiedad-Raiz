156979df932271f0544ab23024ca593d42dfbc1c
Santiago Ramirez Castellanos <santiago5.ramirez@ucp.edu.co>
2024-10-22 12:53:31 -0500

    new Commit Oct 22 V6

--- src/webApp.js
+++ src/webApp.js
@@ -1,12 +1,12 @@
 
 import express from "express";
-import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-
 import authRoutes from "./routes/auth.routes.js";
-import taskRoutes from "./routes/tasks.routes.js";
+import taskRoutes from "./routes/task.routes.js";
+import cors from "cors";
 import { FRONTEND_URL } from "./config.js";
+
 
 const app = express();
 
@@ -24,7 +24,7 @@ app.use("/api", authRoutes);
 app.use("/api", taskRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  const path = await import("path");
+ 
   app.use(express.static("agent/dist"));
 
 

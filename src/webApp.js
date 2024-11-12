076e7ea08c82a9724601275372f77799fe5dbafc
Santiago Ramirez Castellanos <santiago5.ramirez@ucp.edu.co>
2024-11-12 15:32:00 -0500

    nov12

--- src/webApp.js
+++ src/webApp.js
@@ -16,7 +16,7 @@ const __dirname = path.dirname(__filename);
 app.use(
   cors({
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    credentials: 'include',
+    credentials: true,
     origin: "https://easifyview.onrender.com",
   })
 );

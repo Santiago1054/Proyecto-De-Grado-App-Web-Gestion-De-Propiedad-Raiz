e0fb7006739c90e97203d96f238140d376949164
Santiago Ramirez Castellanos <santiago5.ramirez@ucp.edu.co>
2024-11-12 11:45:18 -0500

    deployed

--- agent/src/api/axios.js
+++ agent/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "https://easify.onrender.com"
+const API_URL = "https://easify.onrender.com/api"
 const instance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
